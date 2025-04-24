4de51feec14f31ebeb3912ff9900bfaf0f51fcf6
Shefa2436 <Orehcheto@abv.bg>
2025-04-24 13:09:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -45,5 +45,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 module.exports = app;
